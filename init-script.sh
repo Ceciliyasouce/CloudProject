cc7aff5959e0d9a9471e1078d1572e2c32d05d11
Ceciliyasouce <54509593+Ceciliyasouce@users.noreply.github.com>
2025-06-26 09:32:33 +0200

    Update init-script.sh

--- init-script.sh
+++ init-script.sh
@@ -2,7 +2,24 @@
 
 # Install system packages
 sudo apt-get update -y
-sudo apt-get install -y python3-pip git
+sudo apt upgrade -y
+
+sudo apt install -y build-essential libssl-dev zlib1g-dev \
+libncurses5-dev libncursesw5-dev libreadline-dev \
+libsqlite3-dev libgdbm-dev libdb5.3-dev libbz2-dev \
+libexpat1-dev liblzma-dev tk-dev libffi-dev wget
+
+sudo wget https://www.python.org/ftp/python/3.9.6/Python-3.9.6.tgz
+
+sudo tar -xvzf Python-3.9.6.tgz
+
+cd Python-3.9.6
+
+sudo ./configure --enable-optimizations
+
+make -j $(nproc)
+
+sudo make altinstall
 
 # Clone your GitHub repo
 cd /home/azureuser
